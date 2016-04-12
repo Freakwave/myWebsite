29f9fc6845e3ccd79414116fb4c8aad87eb996ed
Freakwave <mares.eric91@gmail.com>
2016-04-12 12:31:12 +0200

    Recovery

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
 </head>
 <body>
   <div class="container">
@@ -14,8 +16,9 @@
         <strong class="main_head">Headline</strong>
         <p class="sub_head">this is the headline</p>
       </div>
+	  <div class="menu"><i class="fa fa-bars"></i></div>
     </div>
-    <div class="menu"></div>
+    <div class="content"></div>
 
 
   <script src="js/design.js"></script>
--- js/design.js
+++ js/design.js
@@ -1,9 +1,36 @@
 $(window).scroll(function(){
   var scroll = $(this).scrollTop();
-  var wHeight = $(window).height();
-
-  var newHeight = Math.max(50, 400-scroll);
+  var newHeight = Math.max(50, (400-scroll));
   $('.header').css({
     'height' : newHeight+'px'
   });
+
+  if (newHeight <= 300){
+    var newOpacity = Math.max(0, (newHeight-200)/100);
+    $('.sub_head').css({
+      'opacity' : newOpacity
+    });
+  }
+  else {
+    $('.sub_head').css({
+      'opacity' : '1'
+    });
+  }
+  if (newHeight <= 200){
+	  
+    var newWidth = Math.min(95, (-100/150*(newHeight-200)));
+	var newSize = Math.max(0.5, (100-newWidth)/100 );
+    console.log(newWidth);
+    $('.headline').css({
+      'flex-basis' 	: newWidth+'%',
+	  'font-size'	: newSize+'em'
+    });
+  }
+  else
+  {
+    $('.headline').css({
+      'flex-basis' 	: '0%',
+	  'font-size'	: '1em'
+    });
+  }
 });
